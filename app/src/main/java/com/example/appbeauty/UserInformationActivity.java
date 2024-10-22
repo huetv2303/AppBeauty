953a480fe36f3ecc86318f55cb92df7212cb66a2
megumikatou1255 <trnbaooo6996@gmail.com>
2024-10-22 23:03:56 +0700

    bảo fix

---
+++ app/src/main/java/com/example/appbeauty/UserInformationActivity.java
@@ -0,0 +1,14 @@
+package com.example.appbeauty;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class UserInformationActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_profile);
+    }
+}
